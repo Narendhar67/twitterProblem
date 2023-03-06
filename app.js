2200e34174a11470bc894a3852049c8e1247056b
Narendhar <narendhar.mrit467@gmail.com>
2023-03-06 14:45:37 +0000

    server added

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const app = express();
+app.use(express.json());
+
+let db = null;
+const dbPath = path.join(__dirname, "twitterClone.db");
+
+const InitializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Started at http://localhost:3000");
+    });
+  } catch (error) {
+    console.log(`DB Error : ${error.message}`);
+  }
+};
+
+InitializeDBAndServer();
